db11ee2c3d13909a00c7d328cb76709c1682a91a
= <=>
2023-08-08 23:17:41 +0700

    update code v1

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'bysgfeh7',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skill from './skill'
+import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  about,
+  brands,
+  contact,
+  skill,
+  experiences,
+  workExperience,
+]
